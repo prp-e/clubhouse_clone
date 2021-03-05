be134b630adb583acc78dcac6af4d082d8e01939
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-05 03:42:18 +0330

    custom login, register and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   resources :profile 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
