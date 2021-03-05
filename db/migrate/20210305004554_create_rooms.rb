906b41d66009b60269d707d0908caee35767e9bf
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-05 04:16:07 +0330

    Room scaffold added

---
+++ db/migrate/20210305004554_create_rooms.rb
@@ -0,0 +1,11 @@
+class CreateRooms < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rooms do |t|
+      t.string :name
+      t.text :description
+      t.string :meetingID
+
+      t.timestamps
+    end
+  end
+end
