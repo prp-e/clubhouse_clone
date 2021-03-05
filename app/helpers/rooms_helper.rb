b069ac9ce74b36e1bd8334c45b600c31fb85c97d
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-05 12:28:22 +0330

    create a room - skeleton

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,4 @@
 module RoomsHelper
+    def make_room(api_url = ENV['BBB_API'], shared_secret = ENV['BBB_SECRET'], mod_pass = ENV['BBB_MP'], att_pass = ENV['BBB_AP'], room_name, room_id)
+    end 
 end
