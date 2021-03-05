e4cd4dd291518a22213fad721d0b845befc81c7a
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-05 14:36:37 +0330

    Join helpers added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+require 'digest'
+require 'active_support/core_ext/hash'
+
 module ApplicationHelper
+    def join_meeting(api_url = ENV['BBB_API'], shared_secret = ENV['BBB_SECRET'], password, room_id, name)
+        create_checksum = "joinmeetingID=#{room_id}&fullName=#{name}&password=#{password}#{shared_secret}"
+        checksum = Digest::SHA1.hexdigest(create_checksum)
+
+        request_url = api_url + "join?meetingID=#{room_id}&fullName=#{name}&password=#{password}&checksum=#{checksum}"
+        
+        return request_url
+    end 
 end
