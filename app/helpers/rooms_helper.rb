115315c63fb1857adf715b23e8f1161a455357be
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-05 12:33:18 +0330

    Ready to be connected to a BBB instance

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,4 +1,8 @@
+require 'digest'
+require 'active_support/core_ext/hash'
+
 module RoomsHelper
     def make_room(api_url = ENV['BBB_API'], shared_secret = ENV['BBB_SECRET'], mod_pass = ENV['BBB_MP'], att_pass = ENV['BBB_AP'], room_name, room_id)
+
     end 
 end
