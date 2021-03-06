e5283c104f2b83f4b4b8e8c8dd743e3de8f20a55
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-06 22:20:09 +0330

    Working on Follow/Unfollow system

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :rooms
   #devise_for :users
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
-  resources :profile 
+  resources :profile do 
+    resources :follows 
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
