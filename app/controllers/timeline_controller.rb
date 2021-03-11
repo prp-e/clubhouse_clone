1c7995af792b9b889c52f4ccd892e09553c9ee11
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-12 03:14:51 +0330

    Timeline controller finished

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -3,6 +3,18 @@ class TimelineController < ApplicationController
     before_action :find_user 
 
     def index
+        @followings = @user.favorites.find_all
+        @rooms = Room.find_each 
+
+        @rooms_to_show = []
+        @followings.each do |following|
+            @rooms.each do |room|
+                if room.user_id == following.favoritable_id || room.user_id == current_user.id 
+                    @rooms_to_show << room 
+                end 
+            end 
+        end 
+        
     end 
 
     private
