4c8d88b1db0e7f2acb2f6485155b2d64d6ca49e3
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-12 03:10:22 +0330

    Timeline controller in progress

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,2 +1,12 @@
 class TimelineController < ApplicationController
+    before_action :authenticate_user!
+    before_action :find_user 
+
+    def index
+    end 
+
+    private
+    def find_user
+        @user = User.find(current_user.id)
+    end 
 end
